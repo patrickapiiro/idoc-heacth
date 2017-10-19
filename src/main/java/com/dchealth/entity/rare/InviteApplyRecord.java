dc64ef3cf451bef9cb69fb42a34001344d554487
dasinWalk <sunkangqing@163.com>
2017-10-19 13:45:19 +0800

    [skq]1,群组管理实体类提交

---
+++ src/main/java/com/dchealth/entity/rare/InviteApplyRecord.java
@@ -0,0 +1,106 @@
+package com.dchealth.entity.rare;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+/**
+ * Created by Administrator on 2017/10/19.
+ */
+@Entity
+@Table(name = "invite_apply_record", schema = "emhbase", catalog = "")
+public class InviteApplyRecord {
+    private String id;
+    private String groupId;
+    private String userId;
+    private String flag;
+    private String status;
+    private Date createDate;
+
+    @Id
+    @Column(name = "id", nullable = false, length = 64)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "group_id", nullable = true, length = 64)
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    @Basic
+    @Column(name = "user_id", nullable = true, length = 64)
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "flag", nullable = true, length = 2)
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = true, length = 2)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "create_date", nullable = true)
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InviteApplyRecord that = (InviteApplyRecord) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (groupId != null ? !groupId.equals(that.groupId) : that.groupId != null) return false;
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (flag != null ? !flag.equals(that.flag) : that.flag != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (createDate != null ? !createDate.equals(that.createDate) : that.createDate != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (groupId != null ? groupId.hashCode() : 0);
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        result = 31 * result + (flag != null ? flag.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (createDate != null ? createDate.hashCode() : 0);
+        return result;
+    }
+}
