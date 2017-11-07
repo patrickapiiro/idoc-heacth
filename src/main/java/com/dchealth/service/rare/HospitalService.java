c9feb93baf8427730299dd64a3f74ac1b88e193a
dasinWalk <sunkangqing@163.com>
2017-11-07 15:27:39 +0800

    [skq]1,罕见病添加医院保存及查询接口

--- src/main/java/com/dchealth/service/rare/HospitalService.java
+++ src/main/java/com/dchealth/service/rare/HospitalService.java
@@ -1,16 +1,16 @@
 package com.dchealth.service.rare;
 
+import com.dchealth.VO.Page;
 import com.dchealth.entity.common.HospitalDict;
 import com.dchealth.facade.common.BaseFacade;
+import com.dchealth.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,19 +38,58 @@ public class HospitalService {
 
     }
 
+    /**
+     * 获取医院信息
+     * @param hospitalName
+     * @param perPage
+     * @param currentPage
+     * @return
+     */
+    @GET
+    @Path("get-hospital-dicts")
+    public Page<HospitalDict> getAllHospitalDicts(@QueryParam("hospitalName")String hospitalName, @QueryParam("perPage") int perPage,
+                                                  @QueryParam("currentPage") int currentPage){
+        String hql = " from HospitalDict where status<>'-1'";
+        if(!StringUtils.isEmpty(hospitalName)){
+            hql += " and hospitalName like '%"+hospitalName+"%'";
+        }
+        return baseFacade.getPageResult(HospitalDict.class,hql,perPage,currentPage);
+    }
 
     /**
-     * 修改医院信息
+     * 添加,修改,删除医院信息
      *
      * @param hospitalDict
      * @return
      */
     @POST
     @Transactional
-    public Response mergeHospitalDict(HospitalDict hospitalDict) {
+    @Path("merge")
+    public Response mergeHospitalDict(HospitalDict hospitalDict) throws Exception{
+        if(StringUtils.isEmpty(hospitalDict.getId())){//新增
+            String hospitalCode = getMaxHospitalCode();
+            if(!StringUtils.isEmpty(hospitalCode)){
+                hospitalDict.setHospitalCode(hospitalCode);
+            }else{
+                throw new Exception("获取医院编码失败");
+            }
+        }
         HospitalDict mergeHospitalDict = baseFacade.merge(hospitalDict);
         return Response.status(Response.Status.OK).entity(mergeHospitalDict).build();
     }
 
+    /**
+     * 获取医院编码最大值,并对最大值+1
+     * @return
+     */
+    public String getMaxHospitalCode(){
+        String hql = " select max(hospitalCode)+1 from HospitalDict where status<>'-1' and hospitalCode>999";
+        List<Integer> hospitalCodes = baseFacade.createQuery(Integer.class,hql,new ArrayList<Object>()).getResultList();
+        if(hospitalCodes!=null && !hospitalCodes.isEmpty()){
+            return hospitalCodes.get(0)+"";
+        }else{
+            return "";
+        }
+    }
 }
 
