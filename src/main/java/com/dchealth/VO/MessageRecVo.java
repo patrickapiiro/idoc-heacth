e160632f0364922034bdc69c977b613b34483d8b
dasinWalk <sunkangqing@163.com>
2017-11-25 14:59:01 +0800

    [skq]1,站内信代码提交

---
+++ src/main/java/com/dchealth/VO/MessageRecVo.java
@@ -0,0 +1,73 @@
+package com.dchealth.VO;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017/11/9.
+ */
+public class MessageRecVo {
+    private String id;
+    private String title;
+    private String content;
+    private String userName;
+    private String status;
+    private Timestamp createDate;
+
+    public MessageRecVo(String id, String title, String content, String userName, String status, Date createDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userName = userName;
+        this.status = status;
+        this.createDate = new Timestamp(createDate.getTime());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+}
