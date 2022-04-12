43d33de496553f0e45fce1d1bb8c0fbeaaaf799a
Patrick Hayes <patrick@apiiro.com>
2022-04-12 12:02:03 -0400

    rm API

--- src/main/java/com/dchealth/service/TimeService.java
+++ src/main/java/com/dchealth/service/TimeService.java
@@ -44,12 +44,6 @@ public class TimeService {
         return baseFacade.merge(users);
     }
 
-    @GET
-    @Path("get-by-id")
-    public YunUsers getYunUser(@QueryParam("id") String id){
-        return baseFacade.get(YunUsers.class,id);
-    }
-
     @POST
     @Path("userlogin")
     public Response userLogin(){
